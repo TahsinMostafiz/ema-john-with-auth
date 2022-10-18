260822876d401937af0f3a33c2957d3b8d232747
TahsinMostafiz <mostafizur.webdesigner131@gmail.com>
2022-10-18 23:29:48 +0600

    Added Sign In page

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './SignUp.css';
+import { FaGoogle} from 'react-icons/fa';
 
 const SignUp = () => {
     return (
         <div>
             <h2>Sign Up</h2>
+            <form>
+                <div className="form-group">
+                    <label htmlFor="email" >Email</label>
+                    <input type="email" name='email' required/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password" >Password</label>
+                    <input type="password" name='password' required/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="confirm" >Confirm Password</label>
+                    <input type="password" name='confirm' required/>
+                </div>
+                <div>
+                    <button className='btn btn-submit'>Sign Up</button>
+                </div>
+                <p>Already have an account? <Link to="/logIn">Log In</Link> </p>
+                <div className="divider">
+                    <hr />
+                    <span>or</span>
+                    <hr />
+                </div>
+                <div>
+                    <button className='btn btn-google'><FaGoogle /> Continue with Google</button>
+                </div>
+            </form>
         </div>
     );
 };
